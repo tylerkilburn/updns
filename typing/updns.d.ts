4f1bac8c78c5b25678ec38ecf5f945c30024ae4d
wyhaya <wyhaya@gmail.com>
2018-07-25 20:22:09 +0800

    update ts type

--- typing/updns.d.ts
+++ typing/updns.d.ts
@@ -2,9 +2,35 @@
 
 declare namespace updns {
 
-    export function createServer(port: number, address: string): any
+
+    export function createServer(
+        port: number, 
+        address?: string
+    ): EventEmitter
+    
+
+    interface EventEmitter {
+        
+        on(event: 'error', callBack: (
+            error: Error) => void
+        ): EventEmitter
+
+
+        on(event: 'listening', callBack: (
+            server: any) => void  // dgrm.Socket
+        ): EventEmitter
+
+
+        on(event: 'message', callBack: (
+            domain: string, 
+            send: (address: string) => void, 
+            proxy: (address: string) => void
+        ) => void): EventEmitter
+
+    }
 
 }
+
 
 declare module 'updns' {
     export = updns
